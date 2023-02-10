cbd81c57d728502d1026a7c2dc610d722408a362
Varuiza <victor.ruiz.alcala@gmail.com>
2023-02-10 08:46:48 +0100

    Few minor changes to improve readability

--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ declare( strict_types = 1 );
 
 require '../app/App.php';
 require '../app/Sale.php';
-//require '../app/helpers.php';
 
 $app = new App( $argv[1], $argv[2] );
 $app->get_profit_by_category();
